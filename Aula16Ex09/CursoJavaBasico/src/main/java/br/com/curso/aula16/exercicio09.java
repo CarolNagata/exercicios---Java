9e50968965ac171c55c2bba0cbbe1d83ab677247
Carol Nagata <86692269+CarolNagata@users.noreply.github.com>
2022-04-21 13:02:41 -0300

    aula 16 exercicio 09

---
+++ Aula16Ex09/CursoJavaBasico/src/main/java/br/com/curso/aula16/exercicio09.java
@@ -0,0 +1,18 @@
+package br.com.curso.aula16;
+
+import java.util.Scanner;
+
+public class exercicio09 {
+
+	public static void main(String[] args) {
+		
+		//Scanner scan = new Scanner(System.in);
+
+		for (int i = 0; i >= 0 && i < 50; i++ ) {
+			
+			if (i % 2 != 0) {
+			System.out.println("Os números ímpares são: " + i);
+			}
+		}
+	}
+}
