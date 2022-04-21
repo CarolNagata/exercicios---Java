80eeb448c2e494046fc199808cde1c8d04763864
Carol Nagata <86692269+CarolNagata@users.noreply.github.com>
2022-04-21 14:01:06 -0300

    aula 19 exercicio 34

---
+++ Aula19Ex34/CursoJavaBasico/src/main/java/br/com/curso/aula19/exercicio34.java
@@ -0,0 +1,32 @@
+package br.com.curso.aula19;
+
+import java.util.Scanner;
+
+public class exercicio34 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetorA = new int[10];
+
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Digite os números " + i);
+			vetorA[i] = scan.nextInt();
+		}
+
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Analisando o número " + vetorA[i]);
+
+			for (int j = 2; j < vetorA[i]; j++) {
+				if (j % 2 == 0) {
+					System.out.println(j + " é par");
+				}
+			}
+
+			System.out.println();
+		}
+
+	}
+
+}
