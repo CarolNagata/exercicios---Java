e05933e260daac2d74ead7c3bd08e9538dc01e9a
Carol Nagata <86692269+CarolNagata@users.noreply.github.com>
2022-04-21 14:00:53 -0300

    aula 19 exercicio 33

---
+++ Aula19Ex33/CursoJavaBasico/src/main/java/br/com/curso/aula19/exercicio33.java
@@ -0,0 +1,39 @@
+package br.com.curso.aula19;
+
+import java.util.Scanner;
+
+public class exercicio33 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetorA = new int[10];
+
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Digite os números " + i);
+			vetorA[i] = scan.nextInt();
+		}
+
+		boolean primo = true;
+		String msg = "";
+		for (int i = 0; i < vetorA.length; i++) {
+
+			primo = true;
+			for (int j = 2; j < vetorA[i]; j++) {
+				if (vetorA[i] % j == 0) {
+					primo = false;
+					break;
+				}
+			}
+			msg = "";
+			if (primo) {
+				msg = " primo";
+			} else {
+				msg = " não é primo";
+			}
+
+			System.out.println(vetorA[i] + msg);
+		}
+	}
+}
