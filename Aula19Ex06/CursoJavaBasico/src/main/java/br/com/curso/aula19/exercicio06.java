cb6412e2e50eb2d05c8767c1ab25346ff02508c9
Carol Nagata <86692269+CarolNagata@users.noreply.github.com>
2022-04-21 13:46:28 -0300

    aula 19 exercicio 06

---
+++ Aula19Ex06/CursoJavaBasico/src/main/java/br/com/curso/aula19/exercicio06.java
@@ -0,0 +1,43 @@
+package br.com.curso.aula19;
+
+import java.util.Scanner;
+
+public class exercicio06 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int vetorA[] = new int[10];
+		int vetorB[] = new int[vetorA.length];
+		int vetorC[] = new int[vetorA.length];
+
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Entre com a posição do vetorA " + i);
+			vetorA[i] = scan.nextInt();
+		}
+		for (int i = 0; i < vetorB.length; i++) {
+			System.out.println("Entre com a posição do vetorB " + i);
+			vetorB[i] = scan.nextInt();
+			
+			vetorC[i] = vetorA[i] + vetorB[i];
+		}
+		
+		System.out.print("Vetor A = ");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorA[i] + " ");
+		}
+		System.out.println();
+
+		System.out.print("Vetor B = ");
+		for (int i = 0; i < vetorB.length; i++) {
+			System.out.print(vetorB[i] + " ");
+		}
+		System.out.println();
+
+		System.out.print("Vetor C = ");
+		for (int i = 0; i < vetorC.length; i++) {
+			System.out.print(vetorC[i] + " ");
+		}
+	}
+}
