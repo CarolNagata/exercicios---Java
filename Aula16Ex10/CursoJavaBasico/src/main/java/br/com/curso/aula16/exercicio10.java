19955d18974281ce19746f9351f4b8a730abb3a5
Carol Nagata <86692269+CarolNagata@users.noreply.github.com>
2022-04-21 13:03:10 -0300

    aula 16 exercicio 10

---
+++ Aula16Ex10/CursoJavaBasico/src/main/java/br/com/curso/aula16/exercicio10.java
@@ -0,0 +1,20 @@
+package br.com.curso.aula16;
+
+import java.util.Scanner;
+
+public class exercicio10 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro número:");
+		int num1 = scan.nextInt();
+		System.out.println("Digite o segundo número:");
+		int num2 = scan.nextInt();
+		
+		for (int i = num1; i <= num2; i++) {
+				System.out.println(i);
+		}
+	}
+}
