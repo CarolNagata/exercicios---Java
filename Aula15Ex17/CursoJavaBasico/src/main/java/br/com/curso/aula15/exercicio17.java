3212990afc91e68b4959ea51c0927817c4fc8dd3
Carol Nagata <86692269+CarolNagata@users.noreply.github.com>
2022-04-21 11:37:16 -0300

    aula 15 exercicio 17

---
+++ Aula15Ex17/CursoJavaBasico/src/main/java/br/com/curso/aula15/exercicio17.java
@@ -0,0 +1,22 @@
+package br.com.curso.aula15;
+
+import java.util.Scanner;
+
+public class exercicio17 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.println("Entre como ano");
+
+		int ano = scan.nextInt();
+		
+		if ((ano % 400 == 0 || ano % 4 == 0 && ano %100 != 0)) {
+			System.out.println("é bissexto");
+		}else {
+			System.out.println("não é bissexto");
+		}
+	}
+
+}
