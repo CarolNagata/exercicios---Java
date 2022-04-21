a3ced25e5246045ee0ac64507566964a783816c2
Carol Nagata <86692269+CarolNagata@users.noreply.github.com>
2022-04-21 13:01:28 -0300

    aula 16 exercicio 06

---
+++ Aula16Ex06/CursoJavaBasico/src/main/java/br/com/curso/aula16/exercicio06.java
@@ -0,0 +1,19 @@
+package br.com.curso.aula16;
+
+public class exercicio06 {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		int max = 20;
+		
+		System.out.println("Contatndo até  " + max);
+
+		while (i<max) {
+			System.out.print("Valor de i: " + i);
+			i++;
+		}
+	}
+}
+
+
