f89473878895cde745b5b0511c97ca5a0d088e62
Carol Nagata <86692269+CarolNagata@users.noreply.github.com>
2022-04-21 13:48:08 -0300

    aula 19 exercicio 11

---
+++ Aula19Ex11/CursoJavaBasico/src/main/java/br/com/curso/aula19/exercicio11.java
@@ -0,0 +1,32 @@
+package br.com.curso.aula19;
+
+import java.util.Scanner;
+
+public class exercicio11 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetorA = new int[10];
+
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Entre com os múmeros do vetor " + i);
+			vetorA[i] = scan.nextInt();
+		}
+
+		int qtdPares = 0;
+		for (int i = 0; i < vetorA.length; i++) {
+			if (vetorA[i] % 2 == 0) {
+				qtdPares += 1;
+			}
+		}
+		System.out.print("Vetor A = ");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorA[i] + " ");
+		}
+		System.out.println();
+		
+		System.out.println("Quantidade de números pares: " + qtdPares);
+	}
+}
