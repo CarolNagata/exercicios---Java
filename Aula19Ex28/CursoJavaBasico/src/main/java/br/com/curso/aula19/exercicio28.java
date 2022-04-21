bfd1d1ebaa0173bd179953885875a38fefd3f61c
Carol Nagata <86692269+CarolNagata@users.noreply.github.com>
2022-04-21 13:59:32 -0300

    aula 19 exercicio 28

---
+++ Aula19Ex28/CursoJavaBasico/src/main/java/br/com/curso/aula19/exercicio28.java
@@ -0,0 +1,33 @@
+package br.com.curso.aula19;
+
+import java.util.Scanner;
+
+public class exercicio28 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetorA = new int[10];
+		int[] vetorB = new int[vetorA.length];
+
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Entre com os números do vetor A => posição " + i);
+			vetorA[i] = scan.nextInt();
+
+			vetorB[vetorA.length - i - 1] = vetorA[i];
+		}
+		System.out.println();
+
+		System.out.print("Vetor A = ");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorA[i] + " ");
+		}
+		System.out.println();
+
+		System.out.print("Vetor B = ");
+		for (int i = 0; i < vetorB.length; i++) {
+			System.out.print(vetorB[i] + " ");
+		}
+	}
+}
